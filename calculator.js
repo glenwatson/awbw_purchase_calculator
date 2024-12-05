ba964357897541c7266d9d3efef9cdf992fdf5a0
Glen Watson <glen.watson@alumni.neumont.edu>
2024-12-05 12:24:05 -0800

    Adds airports and ports

--- calculator.js
+++ calculator.js
@@ -1,25 +1,44 @@
+let cost_modifier = 1;
+let funds_available = 10500;
+let num_factories = 10;
+let num_airports = 0;
+let num_ports = 5;
+
 const factory_unit_costs = {
-    'Footsolider': 1000,
+    'Infantry': 1000,
     'Mech': 3000,
+    'Recon': 4000,
+    'APC': 5000,
     'Artillery': 6000,
     'Tank': 7000,
     'Anti-Air': 8000,
+    'Missile': 12000,
+    'Rocket': 15000,
+    'Md.Tank': 16000,
+    'Piperunner': 20000,
+    'Neotank': 22000,
+    'Mega Tank': 28000,
 };
 const airport_unit_costs = {
-    'T-Copter': 7000,
+    'T-Copter': 5000,
+    'B-Copter': 9000,
+    'Fighter': 20000,
+    'Bomber': 22000,
+    'Black Bomb': 25000,
+    'Stealth': 24000,
 };
 const port_unit_costs = {
-    'BlackBoat': 1000,
+    'Black Boat': 7500,
+    'Lander': 12000,
+    'Cruiser': 18000,
+    'Sub': 20000,
+    'Battleship': 28000,
+    'Carrier': 30000,
 };
-
-
-let cost_modifier = 1.00;
-
-let funds_available = 10000;
-let num_factories = 10;
-let num_airports = 0;
-let num_ports = 0;
-/** {[funds_available, num_factories. num_airports, num_ports]: [units...] */
+let compareFn = function(unitX, unitY) {
+    return unitX.localeCompare(unitY); // TODO - allow sorting by value, unit type, etc.
+};
+/** Object{[funds_available, num_factories, num_airports, num_ports]: Array<units>} */
 const base_case_records = {};
 
 function get_possible_purchases(unit_cost_lookup, funds) {
@@ -29,66 +48,94 @@ function get_possible_purchases(unit_cost_lookup, funds) {
 }
 
 function goanddoit([funds_available, num_factories, num_airports, num_ports, units]) {
-
-    console.debug(
-        'funds_available', funds_available, 
-        'num_factories', num_factories,
-        'num_airports', num_airports,
-        'num_ports', num_ports,
-        'units', units);
-    const lookup = base_case_records[[funds_available, num_factories, num_airports, num_ports]];
-    if (lookup) {
-        
-        //continue;
+    if ([funds_available, num_factories, num_airports, num_ports] in base_case_records &&
+        base_case_records[[funds_available, num_factories, num_airports, num_ports]].equals(units.sort(compareFn))
+    ) {
+        // skip calculating (duplicating work)
+        return;
     }
-    if (num_factories == 0) {
+    if (num_factories == 0 && num_airports == 0 && num_ports == 0) {
         // base case - all production tiles have been used
-        base_case_records[[funds_available, num_factories, num_airports, num_ports]] = units;
+        base_case_records[[funds_available, num_factories, num_airports, num_ports]] = units.sort(compareFn);
         return;
     }
     const possible_factory_purchases = get_possible_purchases(factory_unit_costs, funds_available);
-    if (possible_factory_purchases.length == 0) {
-        // base case - can't afford anything from the factory
-        base_case_records[[funds_available, num_factories, num_airports, num_ports]] = units;
+    const possible_airport_purchases = get_possible_purchases(airport_unit_costs, funds_available);
+    const possible_port_purchases = get_possible_purchases(port_unit_costs, funds_available);
+    if (possible_factory_purchases.length == 0 && possible_airport_purchases.length == 0 && possible_port_purchases.length == 0) {
+        // base case - can't afford anything from and production tiles
+        base_case_records[[funds_available, num_factories, num_airports, num_ports]] = units.sort(compareFn);
         return;
     }
-    // start with the most expensive purchase
-    for (const factory_purchase_name of possible_factory_purchases) {
-        const new_units = units.slice();
-        new_units.push(factory_purchase_name);
-        goanddoit([funds_available - factory_unit_costs[factory_purchase_name] * cost_modifier, 
-            num_factories - 1,
-            num_airports, 
-            num_ports, 
-            new_units]);
+    // TODO: start with the most expensive purchase
+    if (num_factories > 0) {
+        for (const factory_purchase_name of possible_factory_purchases) {
+            const new_units = units.slice();
+            new_units.push(factory_purchase_name);
+            goanddoit([funds_available - factory_unit_costs[factory_purchase_name] * cost_modifier, 
+                num_factories - 1,
+                num_airports, 
+                num_ports, 
+                new_units]);
+        }
     }
-    // if (num_airports > 0) {
-    //     const possible_airport_purchases = get_possible_purchases(airport_unit_costs, funds_available);
-    //     console.log(possible_airport_purchases);
-    //     if (possible_airport_purchases.length > 0) {
-    //         // start with the most expensive purchase
-    //         const airport_purchase = possible_airport_purchases.at(-1);
-    //         num_airports--;
-    //         funds_available =- airport_purchase[1] * cost_modifier;
-    //         units.push(airport_purchase[0]);
-    //     } else {
-    //         // can't afford anything from the airport
-    //     }
-    // }
-    // if (num_ports > 0) {
-    //     const possible_port_purchases = get_possible_purchases(port_unit_costs, funds_available);
-    //     console.log(possible_port_purchases);
-    //     if (possible_port_purchases.length > 0) {
-    //         // start with the most expensive purchase
-    //         const port_purchase = possible_port_purchases.at(-1);
-    //         num_ports--;
-    //         funds_available =- port_purchase[1] * cost_modifier;
-    //         units.push(port_purchase[0]);
-    //     } else {
-    //         // can't afford anything from the port
-    //     }
-    // }
+    if (num_airports > 0) {
+        for (const airport_purchase_name of possible_airport_purchases) {
+            const new_units = units.slice();
+            new_units.push(airport_purchase_name);
+            goanddoit([funds_available - airport_unit_costs[airport_purchase_name] * cost_modifier, 
+                num_factories,
+                num_airports - 1, 
+                num_ports, 
+                new_units]);
+        }
+    }
+    if (num_ports > 0) {
+        for (const port_purchase_name of possible_port_purchases) {
+            const new_units = units.slice();
+            new_units.push(port_purchase_name);
+            goanddoit([funds_available - port_unit_costs[port_purchase_name] * cost_modifier, 
+                num_factories,
+                num_airports, 
+                num_ports - 1, 
+                new_units]);
+        }
+    }
 }
+
+/* Helper functions */
+// Source: https://stackoverflow.com/questions/7837456/how-to-compare-arrays-in-javascript
+// Warn if overriding existing method
+if(Array.prototype.equals)
+    console.warn("Overriding existing Array.prototype.equals. Possible causes: New API defines the method, there's a framework conflict or you've got double inclusions in your code.");
+// attach the .equals method to Array's prototype to call it on any array
+Array.prototype.equals = function (array) {
+    // if the other array is a falsy value, return
+    if (!array)
+        return false;
+    // if the argument is the same array, we can be sure the contents are same as well
+    if(array === this)
+        return true;
+    // compare lengths - can save a lot of time 
+    if (this.length != array.length)
+        return false;
+
+    for (var i = 0, l=this.length; i < l; i++) {
+        // Check if we have nested arrays
+        if (this[i] instanceof Array && array[i] instanceof Array) {
+            // recurse into the nested arrays
+            if (!this[i].equals(array[i]))
+                return false;       
+        }           
+        else if (this[i] != array[i]) { 
+            // Warning - two different object instances will never be equal: {x:20} != {x:20}
+            return false;   
+        }           
+    }       
+    return true;
+}
+// Hide method from for-in loops
+Object.defineProperty(Array.prototype, "equals", {enumerable: false});
 
 
 goanddoit([funds_available, num_factories, num_airports, num_ports, []]);
